9552bfd9333b83097f898f15a3ad829baa6c1bad
tanvir1017 <tanvir.hossaindev1@gmail.com>
2022-06-09 21:24:29 +0600

    trying to fixed maping

--- app/routes/Blog/index.tsx
+++ app/routes/Blog/index.tsx
@@ -1,20 +1,20 @@
 import { useLoaderData } from "@remix-run/react";
+import * as React from "react";
 import { db } from "~/util/dbConncet.server";
 import type { postDatatype } from "~/util/type.server";
 
 export const loader = async () => {
   const data = {
-    posts: await db.post.findMany({}),
+    posts: await db.post.findMany(),
   };
   return data;
 };
 
 const Index = () => {
-  const { posts } = useLoaderData();
-  console.log(posts);
+  const data = useLoaderData();
   return (
     <>
-      {posts.map((post: postDatatype) => (
+      {data.posts.map((post: postDatatype) => (
         <div key={post._id}>
           <h2>{post.title}</h2>
           <p>{post.body}</p>
